86ce126118e7f770927b8651a4093a839bf0d917
Matt Cole <ccmatt19@gmail.com>
2018-06-28 12:05:51 -0400

    Application has the main functionality I desired.  Need to add details and design page

--- GUI.py
+++ GUI.py
@@ -10,22 +10,35 @@ from matplotlib.figure import Figure
 class App():
 
     def __init__(self, master):
+        df = pd.read_csv('nhl_data.csv')
         frame = ttk.Frame(master)
-        self.values = ['POINTS', 'W', 'GF']
+
+        #title
+        self.title = master.title('NHL Data Analyses')
+
+        #header
+        self.header = Label(frame, text='NHL Data Grapher', fg='red', bg='light blue', font= ('Times New Roman', 55))
+        self.header.pack()
+
+        #choose text
+        self.choose = Label(frame, text='Choose the X and Y axes to see a graph comparing the variables: ')
+        self.choose.pack()
 
         #x axis dropdown
+        ttk.Label(frame, text='X').pack()
         self.x = StringVar()
         self.x_combo = ttk.Combobox(frame, textvariable = self.x)
-        self.x_combo.config(values = self.values)
+        self.x_combo.config(values = list(df))
         self.x_combo.pack()
 
         #y axis dropdown
+        ttk.Label(frame, text='Y').pack()
         self.y = StringVar()
         self.y_combo = ttk.Combobox(frame, textvariable=self.y)
-        self.y_combo.config(values=self.values)
+        self.y_combo.config(values= list(df))
         self.y_combo.pack()
 
-        # hit button to create graph
+        #hit button to create graph
         self.enter = ttk.Button(frame, text='Enter')
         self.enter.pack()
         self.enter.config(command= self.create_graph)
@@ -48,10 +61,6 @@ class App():
         #create figure for graph
         fig = Figure()
         ax = fig.add_subplot(111)
-
-
-
-
 
 
 
