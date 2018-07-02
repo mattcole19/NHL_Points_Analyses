433987ff55301211122a9f76a376358a2377e3ef
Matt Cole <ccmatt19@gmail.com>
2018-07-02 13:24:18 -0400

    Annotated markers

--- GUI.py
+++ GUI.py
@@ -1,10 +1,16 @@
 from tkinter import *
 from tkinter import ttk
 import pandas as pd
-import pandastable as table
-import numpy as np
+from pandastable import Table
 import matplotlib.pyplot as plt
 from matplotlib.figure import Figure
+import random
+
+
+'''count =0
+for team in teams:
+    ax.annotate(team, (x[count], y[count]))
+    count+=1'''
 
 
 class App():
@@ -12,6 +18,7 @@ class App():
     def __init__(self, master):
         df = pd.read_csv('nhl_data.csv')
         frame = ttk.Frame(master)
+        data_frame = ttk.Frame(master)
 
         #title
         self.title = master.title('NHL Data Analyses')
@@ -24,18 +31,19 @@ class App():
         self.choose = Label(frame, text='Choose the X and Y axes to see a graph comparing the variables: ')
         self.choose.pack()
 
+        dataheader = list(df)
         #x axis dropdown
         ttk.Label(frame, text='X').pack()
         self.x = StringVar()
         self.x_combo = ttk.Combobox(frame, textvariable = self.x)
-        self.x_combo.config(values = list(df))
+        self.x_combo.config(values = dataheader[1:])
         self.x_combo.pack()
 
         #y axis dropdown
         ttk.Label(frame, text='Y').pack()
         self.y = StringVar()
         self.y_combo = ttk.Combobox(frame, textvariable=self.y)
-        self.y_combo.config(values= list(df))
+        self.y_combo.config(values= dataheader[1:])
         self.y_combo.pack()
 
         #hit button to create graph
@@ -49,18 +57,23 @@ class App():
     def create_graph(self):
         #dataframe
         df = pd.read_csv('nhl_data.csv')
+        teams = df.ix[:,0]
 
         #get axes
-        x_axis = self.x.get()
-        y_axis = self.y.get()
+        x_axis = df[self.x.get()]
+        y_axis = df[self.y.get()]
 
         #creates scatter plot
-        scatter = df.plot.scatter(x= x_axis, y= y_axis , s= 50, c= 'DarkBlue')
-        plt.show()
+        ax = plt.subplot()
+        ax.scatter(x_axis, y_axis)
 
-        #create figure for graph
-        fig = Figure()
-        ax = fig.add_subplot(111)
+        #puts team name next to respective marker
+        count = 0
+        for team in teams:
+            ax.annotate(team, (x_axis[count], y_axis[count]))
+            count += 1
+
+        plt.show()
 
 
 
